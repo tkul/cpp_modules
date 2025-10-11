80853d91a7ff1361bb0ed35fef10cbba28115eb9
tkul <ttugcekul@gmail.com>
2025-10-11 19:06:27 +0300

    update

--- cpp06/ex02/Base.hpp
+++ cpp06/ex02/Base.hpp
@@ -6,21 +6,6 @@ class Base {
         virtual ~Base() {}
 };
 
-class A : public Base {
-    public:
-        virtual ~A() {}
-};
-
-class B : public Base {
-    public:
-        virtual ~B() {}
-};
-
-class C : public Base {
-    public:
-        virtual ~C() {}
-};
-
 Base* generate(void);
 void identify(Base* p);
 void identify(Base& p);
