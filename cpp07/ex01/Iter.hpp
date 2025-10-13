3ef199b82120b5bab58b48bab524fab9b9d9ef23
tkul <ttugcekul@gmail.com>
2025-10-13 17:58:17 +0300

    update

--- cpp07/ex01/Iter.hpp
+++ cpp07/ex01/Iter.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkul <tkul@student.42istanbul.com.tr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/13 17:16:04 by tkul              #+#    #+#             */
+/*   Updated: 2025/10/13 17:30:03 by tkul             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef ITER_HPP
 #define ITER_HPP
 
@@ -5,6 +17,16 @@
 
 template <typename T>
 void iter(T *array, size_t length, void (*f)(T &)) {
+    if (!array || length == 0 || !f)
+        return;
+    for (size_t i = 0; i < length; i++)
+        f(array[i]);
+}
+
+template <typename T>
+void iter(const T *array, size_t length, void (*f)(const T &)) {
+    if (!array || length == 0 || !f)
+        return;
     for (size_t i = 0; i < length; i++)
         f(array[i]);
 }
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkul <tkul@student.42istanbul.com.tr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/13 17:16:07 by tkul              #+#    #+#             */
+/*   Updated: 2025/10/13 17:57:43 by tkul             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Iter.hpp"
 
 void printInt(int &n) {
@@ -12,10 +24,15 @@ void printChar(char &c) {
     std::cout << c << " ";
 }
 
+void printConstInt(const int &n) {
+    std::cout << n << " ";
+}
+
 int main() {
     int intArray[] = {1, 2, 3, 4, 5};
     float floatArray[] = {1.1f, 2.2f, 3.3f, 4.4f, 5.5f};
     char charArray[] = {'H', 'e', 'l', 'l', 'o'};
+    const int constIntArray[] = {6, 7, 8, 9, 10};
 
     std::cout << "Integer array: ";
     iter(intArray, 5, printInt);
@@ -27,6 +44,10 @@ int main() {
 
     std::cout << "Char array: ";
     iter(charArray, 5, printChar);
+    std::cout << std::endl;
+
+    std::cout << "Const Integer array: ";
+    iter(constIntArray, 5, printConstInt);
     std::cout << std::endl;
 
     return 0;
